d6c7fac99d2a625f180cb3f1e46f40ee9fefd2ed
srikanth161 <attilisrikanth@gmail.com>
2017-10-31 04:05:57 +0800

    Update _callback.php

--- _callback.php
+++ _callback.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta name="salesforce-community" content="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>">
-	<meta name="salesforce-mode" content="<?php echo getenv('SALESFORCE_MODE');?>-callback">
-	<meta name="salesforce-save-access-token" content="true">
-	<meta name="salesforce-allowed-domains" content="<?php echo getenv('SALESFORCE_HEROKUAPP_URL');?>">
-	<script src="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
+	
+    <meta name="salesforce-community" content="https://upworkcommunity-developer-edition.ap5.force.com/Customers">
+    <meta name="salesforce-client-id" content="3MVG9d8..z.hDcPKkUS17WGka1Wk25r3lYcVjiFndnH16rk3BXZndSYgCF46gTvUurKf4xJsS7pHOLR1IyP0D">
+    <meta name="salesforce-mode" content="inline-callback">
+    <meta name="salesforce-save-access-token" content="true">
+    <meta name="salesforce-allowed-domains" content="upworkerapp.herokuapp.com"> 
+    <meta name="salesforce-redirect-uri" content="https://upworkerapp.herokuapp.com/_callback.php">
+    <meta name="salesforce-target" content="#salesforce-login">
+    <meta name="salesforce-login-handler" content="onLogin">
+    <meta name="salesforce-logout-handler" content="onLogout">
+    
+    <script src="https://upworkcommunity-developer-edition.ap5.force.com/Customers/servlet/
+            servlet.loginwidgetcontroller?type=javascipt_widget" async defer></script>
 </head> 
 <body></body>    
 </html>
+
