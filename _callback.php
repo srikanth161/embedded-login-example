ebad8ea2b6d070db544499c4c01d47d0d79ff189
srikanth161 <attilisrikanth@gmail.com>
2017-10-31 05:03:17 +0800

    Update _callback.php

--- _callback.php
+++ _callback.php
@@ -3,11 +3,11 @@
 <head>
 	
     <meta name="salesforce-community" content="https://upworkcommunity-developer-edition.ap5.force.com/Customers">
-    <meta name="salesforce-client-id" content="3MVG9d8..z.hDcPKkUS17WGka1Wk25r3lYcVjiFndnH16rk3BXZndSYgCF46gTvUurKf4xJsS7pHOLR1IyP0D">
+    <meta name="salesforce-client-id" content="3MVG9d8..z.hDcPKkUS17WGka1YSSYI5z.xuM3TDSpRUFYpXpGJE7yr7lXYiZ88KqaS1ocru1JAKL72h9l7rG">
     <meta name="salesforce-mode" content="inline-callback">
     <meta name="salesforce-save-access-token" content="true">
-    <meta name="salesforce-allowed-domains" content="embedded102.herokuapp.com"> 
-    <meta name="salesforce-redirect-uri" content="https://embedded102.herokuapp.com/_callback.php">
+    <meta name="salesforce-allowed-domains" content="embedded202.herokuapp.com"> 
+    <meta name="salesforce-redirect-uri" content="https://embedded202.herokuapp.com/_callback.php">
     <meta name="salesforce-target" content="#salesforce-login">
     <meta name="salesforce-login-handler" content="onLogin">
     <meta name="salesforce-logout-handler" content="onLogout">
